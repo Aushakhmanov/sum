13453c8a6d58f5b34865bfe1f73a0668e7ec2e4f
Ilyas <ilyas_75@mail.ru>
2018-12-20 23:12:36 +0600

    first commit

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("Введите третье число: ");
+            int c = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a+b+c);
+            Console.ReadKey();
+
+        }
+    }
+}
